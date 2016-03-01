01fc87ae6e65591107ec0a19bddc82f1907c76af
Firstein <alienor.brabant@gmail.com>
2016-03-01 12:31:15 +0100

    fix bugs for game.c

--- game.c
+++ game.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include "game.h"
 
+#define SIZE_ARRAY 6
+
+
 struct game_s{
   int nb_pieces;
   int nb_moves;
@@ -13,19 +16,28 @@ game new_game_hr (int nb_pieces, piece *pieces){
   g->nb_pieces = nb_pieces;
   g->nb_moves = 0;
   g->pieces = (piece*)malloc(sizeof(nb_pieces*sizeof(piece*)));
-  for(int i=0; i<nb_pieces; ++i)
+  for(int i=0; i<nb_pieces; ++i){
+    pieces[i] = new_piece_rh(0, 0, true, true);
     copy_piece(pieces[i], g->pieces[i]);
+  }
   return g;
 }
 
 void delete_game (game g){
+  for(int i=0; i<g->nb_pieces; ++i)
+    delete_piece(g->pieces[i]);
   free(g->pieces);
   free(g);
 }
 
 void copy_game (cgame src, game dst){
-  dst = new_game_hr (src->nb_pieces, src->pieces);
-  dst->nb_moves = src->nb_moves;
+  dst->nb_pieces = src->nb_pieces;
+  dst->nb_moves = src->nb_pieces;
+  dst->pieces = (piece*)malloc(sizeof(src->nb_pieces*sizeof(piece*)));
+  for(int i=0; i<src->nb_pieces; ++i){
+    dst->pieces[i] = new_piece_rh(0, 0, true, true);
+    copy_piece(src->pieces[i], dst->pieces[i]);
+  }
 }
 
 int game_nb_piece (cgame g){
@@ -35,7 +47,7 @@ int game_nb_piece (cgame g){
 cpiece game_piece(cgame g, int piece_num){
   if(piece_num <= g->nb_pieces){
     fprintf(stderr, "index is out of range\n");
-    exit(EXIT_FAILURE);
+    return NULL;
   }
   return g->pieces[piece_num];
 }
@@ -44,22 +56,22 @@ bool game_over_hr (cgame g){
   return get_x(g->pieces[0]) == 4 && get_y(g->pieces[0]) == 3;
 }
 
-bool is_in_game(game g, int piece_num){
+bool is_in_game(cgame g, int piece_num){
   cpiece p = g->pieces[piece_num];
-  return get_x(p)>=0 && get_x(p)<6 && get_y(p)>=0 && get_y(p)<6;
+  return get_x(p)>=0 && get_x(p)<SIZE_ARRAY && get_y(p)>=0 && get_y(p)<SIZE_ARRAY;
 }
 
-bool is_above_piece(game g, int piece_num){
+bool is_above_piece(cgame g, int piece_num){
   cpiece p = g->pieces[piece_num];
   for(int i=0; i<piece_num; ++i){
     if(intersect(p,g->pieces[i]))
-      return false;
+      return true;
   }
-  for(int i=0; i<g->nb_pieces; ++i){
+  for(int i=piece_num+1; i<g->nb_pieces; ++i){
     if(intersect(p,g->pieces[i]))
-      return false;
+      return true;
   }
-  return true;
+  return false;
 }
 
 bool play_move(game g, int piece_num, dir d, int distance){
@@ -71,21 +83,23 @@ bool play_move(game g, int piece_num, dir d, int distance){
     if(d == RIGHT || d == LEFT)
       return false;
   }
-  move_piece(p, d, distance);
-  if(is_in_game(g, piece_num) && !is_above_piece(g, piece_num)){
-    g->nb_moves += distance;
-    return true;
-  }else{
-    if(d == RIGHT)
-      move_piece(p, LEFT, distance);
-    else if(d == LEFT)
-      move_piece(p, RIGHT, distance);
-    else if(d == UP)
-      move_piece(p, DOWN, distance);
-    else
-      move_piece(p, UP, distance);
-    return false;
+  int cover_distance;
+  for(cover_distance = 1; cover_distance<=distance; ++cover_distance){
+    move_piece(p, d, 1);
+    if(!is_in_game(g, piece_num) || is_above_piece(g, piece_num)){
+      if(d == RIGHT)
+	move_piece(p, LEFT, cover_distance);
+      else if(d == LEFT)
+	move_piece(p, RIGHT, cover_distance);
+      else if(d == UP)
+	move_piece(p, DOWN, cover_distance);
+      else
+	move_piece(p, UP, cover_distance);
+      return false;
+    }
   }
+  g->nb_moves = distance;
+  return true;
 }
 
 int game_nb_moves(cgame g){
