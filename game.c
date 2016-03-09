cbc9d4ae8792bd329a09bf00644ce52359440c2c
Firstein <alienor.brabant@gmail.com>
2016-03-09 10:20:09 +0100

    rewrite and fix bug to game.c

--- game.c
+++ game.c
@@ -31,11 +31,10 @@ void delete_game (game g){
 }
 
 void copy_game (cgame src, game dst){
-  dst->nb_pieces = src->nb_pieces;
   dst->nb_moves = src->nb_moves;
-  dst->pieces = (piece*)realloc(dst->pieces, sizeof(src->nb_pieces*sizeof(piece*)));
   for(int i=0; i<src->nb_pieces; ++i){
-    dst->pieces[i] = new_piece_rh(0, 0, true, true);
+    if(dst->pieces[i] == NULL)
+      dst->pieces[i] = new_piece_rh(0, 0, true, true);
     copy_piece(src->pieces[i], dst->pieces[i]);
   }
 }
