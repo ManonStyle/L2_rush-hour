b1338d1cc5a4432e480625b95f43192457e03100
GG <gedthekitten@gmail.com>
2016-03-11 22:32:23 +0100

    suite cmake

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/gg/Bureau/projet/rush-hour
+# Build directory: /home/gg/Bureau/projet/rush-hour/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(game)
+subdirs(rush_hour)
---
+++ build/rush_hour/CMakeFiles/rush_hour.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rush_hour.dir/rush_hour.c.o"
+  "rush_hour.pdb"
+  "rush_hour"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/rush_hour.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
